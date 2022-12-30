5ad4b985dc6d5c9d5b41292f56e76df2b58e638d
dhk767 <dhk767@gmail.com>
2022-12-30 16:59:24 +0900

    feat : Home 화면에 TableView 추가

--- CatstaGram/New Group/HomeViewController.swift
+++ CatstaGram/New Group/HomeViewController.swift
@@ -8,22 +8,33 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
     
+    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    // 한 섹션에 몇 개의 셀을 넣을 것인지
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
-
+    
+    // 어떤 셀을 보여줄 것인가
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .facebookColor
+        return cell
+    }
 }
