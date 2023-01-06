5b15cbe719e549133fee3e4b7afacab53d0e59f8
dhk767 <dhk767@gmail.com>
2023-01-07 00:37:26 +0900

    feat : 릴스 UI 배치 완료

--- CatstaGram/UserInterface/Reels/ReelsCollectionViewCell.swift
+++ CatstaGram/UserInterface/Reels/ReelsCollectionViewCell.swift
@@ -15,6 +15,21 @@ class ReelsCollectionViewCell: UICollectionViewCell {
     
     var videoView: VideoPlayerView?
     
+    // 릴스
+    let cellTitleLabel = UILabel()
+    
+    // 카메라
+    let cameraImageView = UIImageView()
+    
+    // 댓글
+    let commentImageView = UIImageView()
+    
+    // 좋아요
+    let likeImageView = UIImageView()
+    
+    // 공유하기
+    let shareImageView = UIImageView()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -30,7 +45,18 @@ class ReelsCollectionViewCell: UICollectionViewCell {
     }
     
     private func setupAttribute() {
+        cellTitleLabel.text = "릴스"
+        cellTitleLabel.font = .boldSystemFont(ofSize: 25)
+        cellTitleLabel.textAlignment = .left
         
+        [cameraImageView, shareImageView, likeImageView, commentImageView].forEach({
+            $0.contentMode = .scaleAspectFit
+            $0.tintColor = .black
+        })
+        cameraImageView.image = UIImage(systemName: "camera")
+        shareImageView.image = UIImage(systemName: "paperplane")
+        commentImageView.image = UIImage(systemName: "message")
+        likeImageView.image = UIImage(systemName: "suit.heart")
     }
     
     private func setupLayout() {
@@ -38,6 +64,41 @@ class ReelsCollectionViewCell: UICollectionViewCell {
         contentView.addSubview(videoView)
         videoView.snp.makeConstraints({ make in
             make.edges.equalToSuperview()
+        })
+        
+        [cellTitleLabel, cameraImageView, likeImageView, commentImageView, shareImageView].forEach({
+            contentView.addSubview($0)
+        })
+        
+        cellTitleLabel.snp.makeConstraints({ make in
+            make.top.equalToSuperview().offset(20)
+            make.leading.equalToSuperview().offset(20)
+        })
+        
+        cameraImageView.snp.makeConstraints({ make in
+            make.top.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.width.height.equalTo(35)
+        })
+        
+        let space = CGFloat(20)
+        
+        shareImageView.snp.makeConstraints({ make in
+            make.bottom.equalToSuperview().offset(-50)
+            make.trailing.trailing.equalToSuperview().offset(-20)
+            make.width.height.equalTo(35)
+        })
+        
+        commentImageView.snp.makeConstraints({ make in
+            make.bottom.equalTo(shareImageView.snp.top).offset(-space)
+            make.centerX.equalTo(shareImageView)
+            make.width.height.equalTo(35)
+        })
+        
+        likeImageView.snp.makeConstraints({ make in
+            make.bottom.equalTo(commentImageView.snp.top).offset(-space)
+            make.centerX.equalTo(shareImageView)
+            make.width.height.equalTo(35)
         })
     }
 }
